b5c8c75c68ab2895b6d5cade7c6dfb25562b6b5a
koko <koko@koko.ko>
2025-05-19 21:15:09 +0300

    prereliz

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,13 @@
 
 #define MOTION_SENSOR_PIN   PB4
 
+uint8_t status = 0;
+
+AccelStepper stepper3(AccelStepper::FULL4WIRE, PA11, PB3, PA15, PA12);// IN1 IN3 IN4 IN2
+//AccelStepper stepper2(AccelStepper::FULL4WIRE, PB9, PB8, PB7, PB6);// IN1 IN3 IN4 IN2
 
 
-#define SS
-#ifdef SS
 
-AccelStepper stepper2(AccelStepper::FULL4WIRE, PB13, PB12, PB14, PB15);// IN1 IN3 IN4 IN2
-AccelStepper stepper3(AccelStepper::FULL4WIRE, PB9, PB8, PB7, PB6);// IN1 IN3 IN4 IN2
 
 
 #define TASK1_STK_SIZE 512
@@ -26,8 +26,25 @@ osThreadDef(task1, osPriorityNormal, 1, TASK1_STK_SIZE);
 void task1(void* pdata) {
   int count = 1;
   while (1) {
-    digitalToggle(LED_BUILTIN);
-    osDelay(80);
+    if(digitalRead(MOTION_SENSOR_PIN)){
+        digitalWrite(LED_BUILTIN, LOW);
+        status = 5;
+            stepper3.move(24800);
+
+        osDelay(5000);
+        
+        digitalWrite(LED_BUILTIN, HIGH);
+        status = 0;
+
+        while(digitalRead(MOTION_SENSOR_PIN)){
+          osDelay(10);
+        }
+    //}else{
+        //status = 0;
+
+    }
+
+    osDelay(30);
   }
 }
 
@@ -37,28 +54,32 @@ osThreadDef(task2, osPriorityNormal, 1, TASK1_STK_SIZE);
 
 void task2(void* pdata) {
   while (1) {
-    stepper2.run();
+    osDelay(1);
+    if(!status)continue;
+
+    ///stepper2.run();
     stepper3.run();
 
-     if (stepper3.distanceToGo() == 0)
-	      stepper3.moveTo(-stepper3.currentPosition());
-    osDelay(1);
+     //if (stepper3.distanceToGo() == 0)
+	   //   stepper3.moveTo(-stepper3.currentPosition());
   }
 }
 
 
 void setup() {
-    stepper2.setMaxSpeed(MAX_SPEED_STEPS_PER_SECOND); // +-400 max
-    stepper2.setAcceleration(100.0);
-    stepper2.move(ONE_TURN_STEPS*10);
+    //stepper2.setMaxSpeed(MAX_SPEED_STEPS_PER_SECOND); // +-400 max
+    //stepper2.setAcceleration(100.0);
+    //stepper2.move(ONE_TURN_STEPS*10);
 
     stepper3.setMaxSpeed(MAX_SPEED_STEPS_PER_SECOND);
     stepper3.setAcceleration(MAX_SPEED_STEPS_PER_SECOND*10);
-    stepper3.moveTo(248);
+    stepper3.move(24800);
 
 
   pinMode(LED_BUILTIN, OUTPUT);
-  
+  pinMode(MOTION_SENSOR_PIN, INPUT);
+
+
   osKernelInitialize();                   // TOS Tiny kernel initialize
   osThreadCreate(osThread(task1), NULL);  // Create task1
   osThreadCreate(osThread(task2), NULL);  // Create task1
@@ -71,37 +92,13 @@ void setup() {
 
 void loop() {
 
-  stepper2.run();
-  stepper3.run();
+  //stepper2.run();
+  //stepper3.run();
 
-     if (stepper3.distanceToGo() == 0)
-	stepper3.moveTo(-stepper3.currentPosition());
+    // if (stepper3.distanceToGo() == 0)
+	//stepper3.moveTo(-stepper3.currentPosition());
 }
 
 
-#endif
 
-#ifdef DD
-void setup() {
-  digitalWrite(PB4, LOW);  // sets the digital pin 13 off
-  pinMode(LED_BUILTIN, OUTPUT);
-
-}
-
-
-void loop() {
-
-  pinMode(PB4, INPUT);    // sets the digital pin 13 as output
-  digitalWrite(LED_BUILTIN, LOW);  // sets the digital pin 13 off
-
-  delay(100);            // waits for a second
-
-  pinMode(PB4, OUTPUT);    // sets the digital pin 13 as output
-  digitalWrite(LED_BUILTIN, HIGH);  // sets the digital pin 13 off
-
-  delay(100);            // waits for a second
-
-}
-
-#endif
 
